5a01c5f276224606945a29d39f4f27b3bcac6589
IRSHAD AHMAD <129326052+Irshad-Ahmaed@users.noreply.github.com>
2024-12-07 14:01:16 +0530

    complted

--- currencyConvertor/src/hooks/useCurrencyInfo.js
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -5,7 +5,7 @@ const useCurrencyInfo = (currency) => {
   const [data, setData] = useState({});
   
   useEffect(()=>{
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
     .then(res => res.json())
     .then(res => {
       console.log(res[currency]);
@@ -21,4 +21,4 @@ const useCurrencyInfo = (currency) => {
   return data;
 }
 
-export default useCurrencyInfo
+export default useCurrencyInfo;
